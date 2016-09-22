ac3cb6c30776796135ecf78e06b646e282d710fa
willriggins <williamriggins2013@gmail.com>
2016-09-21 21:43:07 -0400

    initial import

---
+++ src/sample/User.java
@@ -0,0 +1,43 @@
+package sample;
+
+/**
+ * Created by will on 9/21/16.
+ */
+public class User {
+    String username;
+    String password;
+    boolean loggedIn;
+
+    public User(String username, String password, boolean loggedIn) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = loggedIn;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+}
